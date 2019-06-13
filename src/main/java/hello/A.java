f0fe3a8158912363f65079281849e576f093708b
Metin Yarci <metin.yarci@aurea.com>
2019-06-13 18:10:55 +0300

    revision1

---
+++ src/main/java/hello/A.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A {
+
+    
+    public void x() {
+        
+    
+
+
+
+
+
+
+
+    }
+
+}
